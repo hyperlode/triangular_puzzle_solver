af446f3c35a0259713a128a3ddbf0ab72b8fdd31
Lode Ameije <lode.ameije@bmtcdn.local>
2020-04-30 11:48:41 -0700

    display grid. columns wrong, will change

--- triangular_grid.py
+++ triangular_grid.py
@@ -1,3 +1,5 @@
+import traceback
+
 CELL_FAKE = 0  # for programatorical reasons
 CELL_NOGO = 1  # not allowed --> i.e. board edge 
 CELL_ON = 2
@@ -31,7 +33,11 @@ class TriangularGrid():
                         g += " /\\ "
                     else:
                         g += "\\EE/"
-                        
+                elif self._cells[(row,col)] == CELL_ON:
+                    if row % 2 == 0:
+                        g += " /\\ "
+                    else:
+                        g += "\\* /"
                 else:
                     if row % 2 == 0:
                         g += " /\\ "
@@ -45,6 +51,13 @@ class TriangularGrid():
                         g += "/EE\\"
                     else:
                         g += "_\\/_"
+                elif self._cells[(row,col)] == CELL_ON:
+                    if row % 2 == 0:
+                        g += "/_*\\"
+                    else:
+                        g += "_\\/_"
+                
+                
                 else:
                     if row % 2 == 0:
                         g += "/__\\"
@@ -53,13 +66,29 @@ class TriangularGrid():
             g += "\n"
         return g
 
-    def set_cell(cell, on_else_off):
+    def set_cell(self, cell, on_else_off):
         value = CELL_OFF
         if on_else_off:
             value = CELL_ON
+
+        if self._cells[cell] == CELL_FAKE:
+            raise IllegalCellException
+
+        if self._cells[cell] == CELL_NOGO:
+            raise NoGoCellException
+        
         self._cells[cell] = value
 
 
 if __name__ == "__main__":
     g = TriangularGrid(10,14)
+    try:
+        # g.set_cell((0,0),CELL_ON)
+        # g.set_cell((1,1),CELL_ON)
+        pass
+    except Exception as e:
+        print("Failed to pass cells test. {}{}".format(e, traceback.format_exc()))
+
+    g.set_cell((4,4),CELL_ON)
+    g.set_cell((4,5),CELL_ON)
     print(str(g))
