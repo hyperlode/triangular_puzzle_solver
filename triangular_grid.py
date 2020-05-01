1121daf27cb387c8ef6833d05b4f370b043fed9d
Lode Ameije <lode.ameije@bmtcdn.local>
2020-04-30 17:23:04 -0700

    Create and debug triangular grid with cells

--- triangular_grid.py
+++ triangular_grid.py
@@ -5,6 +5,8 @@ CELL_NOGO = 1  # not allowed --> i.e. board edge
 CELL_ON = 2
 CELL_OFF = 3
 
+display_fill = {CELL_FAKE:"!", CELL_NOGO:"-", CELL_ON:"O", CELL_OFF:" "}
+
 class TriangularGrid():
 
     def __init__(self, rows, cols):
@@ -37,23 +39,50 @@ class TriangularGrid():
                 cell_disp = []
                 even_col = col % 2 == 0
 
+                if self._cells[(row,col)] == CELL_FAKE:
+
+                    disp_fill_current = display_fill[CELL_FAKE]
+                    disp_fill_neighbour_left = display_fill[CELL_FAKE]
+                    disp_fill_neightbour_right = display_fill[CELL_FAKE]
+                else:
+                     disp_fill_current = display_fill[self._cells[(row,col)]]
+                     disp_fill_neighbour_left = display_fill[self._cells[(row,col-1)]]
+                     disp_fill_neightbour_right = display_fill[self._cells[(row,col+1)]]
+
+
                 if even_row:
                     if even_col:
-                        cell_disp.append("  /")
-                        cell_disp.append(" / ")
+                        # left: col -1 
+                        # right: current
+                        cell_disp.append("{}{}/".format(disp_fill_neighbour_left,disp_fill_neighbour_left))
+                        cell_disp.append("{}/{}".format(disp_fill_neighbour_left, disp_fill_current))
                         cell_disp.append("/__")
                     else:
-                        cell_disp.append("\\  ")
-                        cell_disp.append(" \\ ")
+                        # left : col-1
+                        # right: current 
+                        cell_disp.append("\\{}{}".format(
+                            disp_fill_current,disp_fill_current))
+                        cell_disp.append("{}\\{}".format(
+                            disp_fill_neighbour_left, disp_fill_current))
                         cell_disp.append("__\\")
                 else:
                     if even_col:
-                        cell_disp.append("\\  ")
-                        cell_disp.append(" \\ ")
+                        # left  : col -1 
+                        # right: current
+                        
+                        cell_disp.append("\\{}{}".format(
+                            disp_fill_current,disp_fill_current))
+                        cell_disp.append("{}\\{}".format(
+                            disp_fill_neighbour_left, disp_fill_current))
                         cell_disp.append("__\\")
                     else:
-                        cell_disp.append("  /")
-                        cell_disp.append(" / ")
+                        # right: current cell
+                        # left: col - 1 
+                        
+                        cell_disp.append("{}{}/".format(
+                            disp_fill_neighbour_left,disp_fill_neighbour_left))
+                        cell_disp.append("{}/{}".format(
+                            disp_fill_neighbour_left, disp_fill_current))
                         cell_disp.append("/__")
 
                 row_disp.append(cell_disp)
@@ -70,43 +99,6 @@ class TriangularGrid():
                 grid_str += "\n"
 
         return grid_str
-                # if self._cells[(row,col)] == CELL_FAKE:
-                #     if row % 2 == 0:
-                #         g += " /\\ "
-                #     else:
-                #         g += "\\EE/"
-                # elif self._cells[(row,col)] == CELL_ON:
-                #     if row % 2 == 0:
-                #         g += " /\\ "
-                #     else:
-                #         g += "\\* /"
-                # else:
-        #             if row % 2 == 0:
-        #                 g += " /\\ "
-        #             else:
-        #                 g += "\\  /"
-                        
-        #     g += "\n"
-        #     for col in range(self._cols):
-        #         if self._cells[(row,col)] == CELL_FAKE:
-        #             if row % 2 == 0:
-        #                 g += "/EE\\"
-        #             else:
-        #                 g += "_\\/_"
-        #         elif self._cells[(row,col)] == CELL_ON:
-        #             if row % 2 == 0:
-        #                 g += "/_*\\"
-        #             else:
-        #                 g += "_\\/_"
-                
-                
-        #         else:
-        #             if row % 2 == 0:
-        #                 g += "/__\\"
-        #             else:
-        #                 g += "_\\/_"
-        #     g += "\n"
-        # return g
 
     def set_cell(self, cell, on_else_off):
         value = CELL_OFF
@@ -129,8 +121,11 @@ if __name__ == "__main__":
         # g.set_cell((1,1),CELL_ON)
         pass
     except Exception as e:
-        print("Failed to pass cells test. {}{}".format(e, traceback.format_exc()))
+        print("Failed to iopass cells test. {}{}".format(e, traceback.format_exc()))
 
-    g.set_cell((4,4),CELL_ON)
-    g.set_cell((4,5),CELL_ON)
+    g.set_cell((2,3),CELL_ON)
+    g.set_cell((3,3),CELL_ON)
+    g.set_cell((3,4),CELL_ON)
+
+    # g.set_cell((4,5),CELL_ON)
     print(str(g))
